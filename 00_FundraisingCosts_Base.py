3bd6c5756192325f9d42ca06b7ba007744667246
alis70929 <60583162+alis70929@users.noreply.github.com>
2021-04-09 12:20:09 +1200

    added 02 to base

--- 00_FundraisingCosts_Base.py
+++ 00_FundraisingCosts_Base.py
@@ -13,7 +13,28 @@ def num_check(question, error, num_type):
             print(error)
 
 
+def yes_no(question):
+    to_check = ["yes", "no"]
+
+    valid = False
+    while not valid:
+
+        response = input(question).lower()
+
+        for item in to_check:
+            if response == item:
+                return item
+            elif response == item[0]:
+                return item
+
+        print("Please enter yes or no")
+
+
 # ***************** Main Routine **************
+for item in range(0, 6):
+    want_help = yes_no("Do you want to read the instructions: ")
+    print("You said {}".format(want_help))
+
 get_int = num_check("How many do you need? ",
                     "Please enter a whole number more than 0 \n",
                     int)
