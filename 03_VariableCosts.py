eec65f2a04fbb1642eee6ff9c9176008f8dd8136
alis70929 <60583162+alis70929@users.noreply.github.com>
2021-04-13 09:54:34 +1200

    finfihed variable cost

--- 03_VariableCosts.py
+++ 03_VariableCosts.py
@@ -37,9 +37,9 @@ price_list = []
 
 variable_cost_dict = {
 
-    'Item Name' = item_list,
-    'Amount' = quantity_list,
-    'Price' = price_list,
+    'Item Name': item_list,
+    'Quantity': quantity_list,
+    'Price': price_list,
 
 }
 
@@ -53,15 +53,31 @@ while item_name != "xxx":
     item_name = not_blank("Item name: ",
                           "The component name can't be blank")
 
-    if item.name.lower() = "xxx":
+    if item_name.lower() == "xxx":
         break
 
-        amount = num_check("Amount: ",
-                           "The amount must be a whole number greater than zero",
-                           int)
-        price = num_check("Price:",
-                          "The amount must be greater than zero",
-                          float)
+    quantity = num_check("Quantity: ",
+                         "The amount must be a whole number greater than zero",
+                         int)
+    price = num_check("Price:",
+                      "The amount must be greater than zero",
+                      float)
 
-        item_list.appen
+    item_list.append(item_name)
+    quantity_list.append(quantity)
+    price_list.append(price)
 
+variable_cost_frame = pandas.DataFrame(variable_cost_dict)
+variable_cost_frame = variable_cost_frame.set_index('Item Name')
+
+variable_cost_frame['Cost'] = variable_cost_frame['Quantity'] * variable_cost_frame['Price']
+
+variable_cost_sub = variable_cost_frame['Cost'].sum()
+
+add_dollars = ['Price', "Cost"]
+for item in add_dollars:
+    variable_cost_frame[item] = variable_cost_frame[item].apply(currency)
+
+print(variable_cost_frame)
+print()
+print("Sub Total: ${:.2f}".format(variable_cost_sub))
