0caaa0f60974440aa3975fd7872a86adb5a5de78
alis70929 <60583162+alis70929@users.noreply.github.com>
2021-05-10 10:48:41 +1200

    finsihed export

---
+++ 07_export_file_v1.py
@@ -0,0 +1,46 @@
+import pandas
+
+variable_dict = {
+    "Item": ["Mugs", "Printing", "Packaging"],
+    "Quantity": [300,300,50],
+    "Price": [1,.5,.75]
+}
+
+fixed_dict = {
+    "Item": ["Rent", "Artwork", "Packaging"],
+    "Price":[25,35,10]
+}
+
+variable_cost_frame = pandas.DataFrame(variable_dict)
+fixed_cost_frame = pandas.DataFrame(fixed_dict)
+
+product_name = "Custom Mugs"
+profit_target = "$100.00"
+required_sales = "$200.00"
+recommended_price = "$5.00"
+
+# Chnage data frame to string so it can be written to txt file
+variable_cost_txt = pandas.DataFrame.to_string(variable_cost_frame)
+fixed_cost_txt = pandas.DataFrame.to_string(fixed_cost_frame)
+
+#list holding stuff to print/write to file
+to_write = [product_name,variable_cost_txt,fixed_cost_txt,profit_target,required_sales,recommended_price]
+
+
+# Wirte to file
+#create file to hold data add .txt extension 
+file_name = "{}.txt".format(product_name)
+text_file = open(file_name, "w+")
+
+# write items to file
+for item in to_write:
+    text_file.write(item)
+    text_file.write("\n\n")
+
+# Close Text File
+text_file.close()
+
+# print the stuff
+for item in to_write:
+    print(item)
+    print()
